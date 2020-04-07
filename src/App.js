9870490ecd6e68177068602021cbd8665f52f471
Stanley Chen <dannyshien@gmail.com>
2020-04-07 18:10:31 -0400

    Initial commit for 30/30 challenge

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Simple Todo App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
