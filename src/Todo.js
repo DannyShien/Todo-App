da8c4801525d0bbf7ced5e1753a613de8598b867
Stanley Chen <dannyshien@gmail.com>
2020-04-07 23:48:35 -0400

    Day1. Simple Todo app that currenlty only adds to todo list.

--- src/Todo.js
+++ src/Todo.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react';
 import './Todo.css';
 
-// Add task to list
-// Display tasks
-// Delete tasks 
 
-class Todo extends React {
+class Todo extends Component {
   constructor(props) {
     super(props); 
     this.state = {
@@ -17,12 +14,59 @@ class Todo extends React {
   render() {
     return (
       <div className="Todo">
-        <header className="Todo-header">
-          <h1>Simple Todo App</h1>
-        </header>
+        <h1>Simple Todo App</h1>
+        <form onSubmit={ this.handleSubmit } >
+          <input
+            text='text'
+            name='input'
+            placeholder='Let`s add a task..'
+            value={this.state.input}
+            onChange={ this.handleInput }
+          />
+          <button
+            type='submit'
+          >Add</button>
+        </form>
+        <div>
+          {this.state.todoList.map( todo => (
+            <div>{todo}</div>
+          ))}
+        </div>
+
       </div>
     )
+  } 
+
+  handleInput = (event) => {
+
+    this.setState({
+      [event.target.name] : event.target.value
+    })
   }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(`Adding task`)
+    const { input } = this.state
+    console.log(`TASK ADDED:`,input)
+    this.setState({
+      todoList:  [input, ...this.state.todoList]
+    })
+    this.setState({
+      input: ''
+    })
+  }
+  
+  // renderTask = () => {
+  //   const { todoList } = this.state.todoList
+  //   console.log(todoList)
+  //   return (
+  //     <ul>
+  //       <li></li>
+  //     </ul>
+  //   )
+  // }
+
 }
 
 export default Todo;
